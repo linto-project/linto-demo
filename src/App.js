65522de224e44ef6d52f3e3312c95bc78651a590
Gautier Arcin <gauta82@gmail.com>
2020-11-25 13:18:35 +0100

    add a router to link to another demo

--- src/App.js
+++ src/App.js
@@ -1,69 +1,34 @@
-import { useState, useEffect } from "react";
-import Button from "./components/Button";
 import "./App.css";
 
-import imageVideo from "./images/video.png";
-import imageAudioVideo from "./images/audiovideo.png";
-import imageAudio from "./images/audio.png";
-import imageNormal from "./images/normal.png";
+import Demo from "./Pages/Demo";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  const [button, setButton] = useState([]);
-
-  const toggleFilter = (filter) => {
-    if (button.includes(filter)) {
-      setButton(button.filter((t) => t !== filter));
-    } else {
-      setButton([...button, filter]);
-    }
-  };
-
-  const isButtonSelected = (p) => button.includes(p);
-
-  useEffect(() => {
-    console.log("button: ", button);
-    // eslint-disable-next-line
-  }, [button]);
-
   return (
-    <div className="app">
-      <div classname="grid">
-        {!isButtonSelected("video") && !isButtonSelected("audio") && (
-          // eslint-disable-next-line
-          <img src={imageNormal} width="800" height="665" />
-        )}
-        {!isButtonSelected("video") && isButtonSelected("audio") && (
-          // eslint-disable-next-line
-          <img src={imageAudio} width="800" height="665" />
-        )}
-        {isButtonSelected("video") && !isButtonSelected("audio") && (
-          // eslint-disable-next-line
-          <img src={imageVideo} width="800" height="665" />
-        )}
-        {isButtonSelected("video") && isButtonSelected("audio") && (
-          // eslint-disable-next-line
-          <img src={imageAudioVideo} width="800" height="665" />
-        )}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/demo1">Demo1</Link>
+            </li>
+            <li>
+              <Link to="/about">Demo2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/demo1">
+            <Demo />
+          </Route>
+          <Route path="/about"></Route>
+        </Switch>
       </div>
-      <div className="grid">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => toggleFilter("video")}
-          selected={isButtonSelected("video")}
-        >
-          Détection de locuteur audio
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => toggleFilter("audio")}
-          selected={isButtonSelected("audio")}
-        >
-          Détection de locuteur vidéo
-        </Button>
-      </div>
-    </div>
+    </Router>
   );
 };
 
