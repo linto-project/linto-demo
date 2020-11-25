0da5f49c816ea3e8d13a0595ce2de9d2b55171e7
Gautier Arcin <gauta82@gmail.com>
2020-11-25 11:41:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Button from "./components/Button";
+import "./App.css";
 
-function App() {
+import imageVideo from "./images/video.png";
+import imageAudioVideo from "./images/audiovideo.png";
+import imageAudio from "./images/audio.png";
+import imageNormal from "./images/normal.png";
+
+const App = () => {
+  const [button, setButton] = useState([]);
+
+  const toggleFilter = (filter) => {
+    if (button.includes(filter)) {
+      setButton(button.filter((t) => t !== filter));
+    } else {
+      setButton([...button, filter]);
+    }
+  };
+
+  const isButtonSelected = (p) => button.includes(p);
+
+  useEffect(() => {
+    console.log("button: ", button);
+    // eslint-disable-next-line
+  }, [button]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div classname="grid">
+        {!isButtonSelected("video") && !isButtonSelected("audio") && (
+          <img src={imageNormal} width="800" height="700" />
+        )}
+        {!isButtonSelected("video") && isButtonSelected("audio") && (
+          <img src={imageAudio} width="800" height="700" />
+        )}
+        {isButtonSelected("video") && !isButtonSelected("audio") && (
+          <img src={imageVideo} width="800" height="700" />
+        )}
+        {isButtonSelected("video") && isButtonSelected("audio") && (
+          <img src={imageAudioVideo} width="800" height="700" />
+        )}
+      </div>
+      <div className="grid">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => toggleFilter("video")}
+          selected={isButtonSelected("video")}
         >
-          Learn React
-        </a>
-      </header>
+          Détection de locuteur audio
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => toggleFilter("audio")}
+          selected={isButtonSelected("audio")}
+        >
+          Détection de locuteur vidéo
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
